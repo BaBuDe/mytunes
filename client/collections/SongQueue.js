8328b724b2190b396f93a225d627e84dd2daf1da
C. Moss <caly.moss@gmail.com>
2014-06-19 19:07:34 -0700

    22 tests now passing

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,35 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
+
 var SongQueue = Songs.extend({
 
   initialize: function(){
+  	this.on('add', this.enqueue, this);
+  	this.on('dequeue', this.dequeue, this);
+  	this.on('ended', this.playNext, this); 
+  },
+
+  enqueue: function(song) {
+  	if (this.length === 1) {
+  		this.playFirst();
+  	}
+  },
+
+  dequeue: function(song) {
+  	this.remove(song);
+  	if (this.length === 0) {
+  		this.trigger('stop');
+  	}
+  },
+  
+  playFirst: function() {
+    this.at(0).play();
+  },
+
+  playNext: function() {
+  	this.shift();
+  	if (this.length >= 1) {
+  		this.playFirst();
+  	}
   }
 
 });
--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -1,11 +1,28 @@
 // PlayerView.js - Defines a backbone view class for the music player.
+//  Here when the song ends (triggered), then dequeue, and play the 
+// next song
+// Call setSong, get access to songs queue, 
 var PlayerView = Backbone.View.extend({
+
 
   // HTML5 (native) audio tag is being used
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
+
   el: '<audio controls autoplay />',
 
-  initialize: function() {
+  events: {
+    'ended' : 'ended'
+  },
+
+  //event lilsteners can only be attached to child elements of el
+
+  ended: function () {
+    // this.trigger('ended', this);
+    // dequeue this song
+    this.model.dequeue();
+
+    // play next song  
+    this.model.play();
   },
 
   setSong: function(song){
